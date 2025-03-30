a87daaa39f2329f0d4a728f6e12d3b26266ea42e
anno <scs_lo_ol_728@icloud.com>
2025-03-30 18:13:17 +0900

    画面のレンダリングがおかしかったので、修正

--- app/likes.tsx
+++ app/likes.tsx
@@ -2,6 +2,7 @@
 
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useRouter } from "next/navigation";
+import { startTransition } from "react";
 
 export default function Likes({
   tweet,
@@ -17,22 +18,21 @@ export default function Likes({
       data: { user },
     } = await supabase.auth.getUser();
     if (user) {
+      const newTweet = {
+        ...tweet,
+        likes: tweet.user_has_liked_tweet ? tweet.likes - 1 : tweet.likes + 1,
+        user_has_liked_tweet: !tweet.user_has_liked_tweet,
+      };
+      startTransition(() => {
+        addOptimisticTweet(newTweet);
+      });
+
       if (tweet.user_has_liked_tweet) {
-        addOptimisticTweet({
-          ...tweet,
-          likes: tweet.likes - 1,
-          user_has_liked_tweet: !tweet.user_has_liked_tweet,
-        });
         await supabase.from("likes").delete().match({
           tweet_id: tweet.id,
           user_id: user.id,
         });
       } else {
-        addOptimisticTweet({
-          ...tweet,
-          likes: tweet.likes + 1,
-          user_has_liked_tweet: !tweet.user_has_liked_tweet,
-        });
         await supabase.from("likes").insert({
           tweet_id: tweet.id,
           user_id: user.id,
