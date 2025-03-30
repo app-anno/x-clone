17b56e75eacde831137f3ddaa987b6fa72cbebb5
anno <scs_lo_ol_728@icloud.com>
2025-03-30 18:27:05 +0900

    18 Supabase Realtime を使用してデータベースの変更に応じて UI を動的に更新する

--- app/tweets.tsx
+++ app/tweets.tsx
@@ -1,6 +1,8 @@
 "use client";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import Likes from "./likes";
-import { useOptimistic } from "react";
+import { useEffect, useOptimistic } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Tweets({ tweets }: { tweets: TweetWithAuthor[] }) {
   const [optimisticTweets, updateOptimisticTweets] = useOptimistic<
@@ -17,6 +19,25 @@ export default function Tweets({ tweets }: { tweets: TweetWithAuthor[] }) {
       return newOptimisticTweets;
     }
   );
+
+  const supabase = createClientComponentClient<Database>();
+  const router = useRouter();
+
+  useEffect(() => {
+    const channel = supabase
+      .channel("realtime tweets")
+      .on(
+        "postgres_changes",
+        { event: "*", schema: "public", table: "tweets" },
+        () => {
+          router.refresh();
+        }
+      )
+      .subscribe();
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, [supabase, router]);
 
   return optimisticTweets?.map((tweet) => (
     <div key={tweet.id}>
